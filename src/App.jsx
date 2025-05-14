126f5343df1227c912fae0feb68c7527b6200ba0
Steve Sewell <steve@builder.io>
2025-05-14 08:29:34 -0600

    new homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,22 @@
-import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import CardDemo from './pages/CardDemo'
-import MyApps from './pages/MyApps'
-import MyAccount from './pages/MyAccount'
-import { FluentThemeProvider } from './theme/ThemeProvider'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+
+import CardDemo from "./pages/CardDemo";
+import MyApps from "./pages/MyApps";
+import MyAccount from "./pages/MyAccount";
+import { FluentThemeProvider } from "./theme/ThemeProvider";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <FluentThemeProvider>
       <Routes>
         <Route path="/myapps" element={<MyApps />} />
         <Route path="/myaccount" element={<MyAccount />} />
         <Route path="/card-demo" element={<CardDemo />} />
-        <Route path="/" element={
-          <>
-            <div>
-              <a href="https://vite.dev" target="_blank">
-                <img src={viteLogo} className="logo" alt="Vite logo" />
-              </a>
-              <a href="https://react.dev" target="_blank">
-                <img src={reactLogo} className="logo react" alt="React logo" />
-              </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-              <button onClick={() => setCount((count) => count + 1)}>
-                count is {count}
-              </button>
-              <p>
-                Edit <code>src/App.jsx</code> and save to test HMR
-              </p>
-            </div>
-            <p className="read-the-docs">
-              Click on the Vite and React logos to learn more
-            </p>
-          </>
-        } />
+        <Route path="/" element={<MyApps />} />
       </Routes>
     </FluentThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
